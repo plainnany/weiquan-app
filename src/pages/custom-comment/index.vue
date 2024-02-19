28bd520ad076a3374959628c64c000489f6112a6
plainnany <plainnany@gmail.com>
2024-02-19 18:34:17 +0800

    feat: 客诉添加左右滑动效果

--- src/pages/custom-comment/index.vue
+++ src/pages/custom-comment/index.vue
@@ -1,12 +1,12 @@
 <template>
   <view class="custom-comment">
     <view class="custom-tabs">
-      <view class="custom-tab" :class="{ active: active === tab.key }" @tap="clickTab(tab)" v-for="tab in tabs" :key="tab.key">
+      <view class="custom-tab" :class="{ active: activeIndex === tab.index }" @tap="clickTab(tab)" v-for="tab in tabs" :key="tab.key">
         {{ tab.title }}</view
       >
     </view>
     <view class="custom-add-btn" @tap="handleClick({})">+</view>
-    <view class="custom-record">
+    <view class="custom-record" @touchstart="touchstart" @touchmove="touchmove" @touchend="touchend">
       <scroll-view :scroll-y="true" @scrolltolower="toLower">
         <view class="custom-list" v-if="complainList.length > 0">
           <view class="custom-list-item" v-for="(item, index) in complainList" :key="index" @tap="handleClick(item)">
@@ -33,16 +33,19 @@ export default {
   data() {
     return {
       active: 'todo',
+      activeIndex: 0,
       tabs: [
         {
           title: '未处理',
           key: 'todo',
           state: '02',
+          index: 0,
         },
         {
           title: '已处理',
           key: 'done',
           state: '03',
+          index: 1,
         },
       ],
       complainList: [],
@@ -50,6 +53,7 @@ export default {
       complete: false,
       pageNo: 1,
       fetchOrder: 0,
+      startX: '',
     }
   },
   mounted() {
@@ -58,12 +62,20 @@ export default {
   onShow() {
     this.getComplainList()
   },
+  watch: {
+    activeIndex: {
+      immediate: true,
+      handler(val) {
+        this.complete = false
+        this.pageNo = 1
+        this.getComplainList()
+      },
+    },
+  },
   methods: {
-    clickTab({ key }) {
+    clickTab({ key, index }) {
       this.active = key
-      this.complete = false
-      this.pageNo = 1
-      this.getComplainList()
+      this.activeIndex = index
     },
     getComplainList(type) {
       const state = this.tabs.find(tab => tab.key === this.active).state
@@ -103,6 +115,38 @@ export default {
       } else {
         Taro.navigateTo({ url: `/pages/comment/index?type=add` })
       }
+    },
+    touchstart(e) {
+      this.moving = true
+      if (e.touches.length == 1) {
+        this.startX = e.touches[0].clientX
+      }
+    },
+    touchmove(e) {
+      if (!this.moving) return
+      if (e.touches.length == 1) {
+        const moveX = e.touches[0].clientX
+        const disX = this.startX - moveX
+        if (Math.abs(disX) > 5) {
+          // 向左滑动
+          if (disX > 0) {
+            this.moving = false
+            this.activeIndex++
+            if (this.activeIndex === this.tabs.length) {
+              this.activeIndex = this.tabs.length - 1
+            }
+          } else {
+            this.moving = false
+            this.activeIndex--
+            if (this.activeIndex <= 0) {
+              this.activeIndex = 0
+            }
+          }
+        }
+      }
+    },
+    touchend(e) {
+      this.moving = false
     },
   },
 }
